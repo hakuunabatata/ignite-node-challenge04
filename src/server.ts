c2587a3d4577e4f27201618c79a2f57d10f129e6
hakuunabatata <emachadomartins1999@gmail.com>
2021-08-15 16:27:42 -0300

    add swagger

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
+import swaggerUiExpress from "swagger-ui-express";
+
 import { app } from ".";
+import swaggerFile from "./swagger.json";
+
+app.use(
+  "/api-docs",
+  swaggerUiExpress.serve,
+  swaggerUiExpress.setup(swaggerFile)
+);
 
 app.listen(3333, () => console.log("Server is running!"));
